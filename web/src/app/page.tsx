a3c0190a27e79ddadd341ec5ac71a0582c33d9b1
Dimitrije Glibic <dimitrije.glibic1@gmail.com>
2025-04-11 01:44:43 +0200

    Fix page container classes

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,10 +2,10 @@ import GenerateForm from '@/components/GenerateForm';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="">
+      <main className="">
         <section>
-          <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+          <div className="py-8 px-4 mx-auto max-w-screen-lg">
             <h1 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
               Start generating your code
             </h1>

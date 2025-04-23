5aff7423f9b7a86788eefcb037c679da612d344c
Dimitrije Glibic <dimitrije.glibic1@gmail.com>
2025-04-23 17:25:33 +0200

    Add dbml language

--- web/src/components/features/DBMLCodeEditor/DBMLCodeEditor.tsx
+++ web/src/components/features/DBMLCodeEditor/DBMLCodeEditor.tsx
@@ -7,18 +7,39 @@ const MonacoEditor = dynamic(() => import('@monaco-editor/react'), {
 
 interface DBMLCodeEditorProps {
   dbmlCode: string;
+  // eslint-disable-next-line no-unused-vars
   onDbmlCodeChange: (value: string | undefined) => void;
 }
 
-const DBMLCodeEditor = ({ dbmlCode, onDbmlCodeChange }: DBMLCodeEditorProps) => (
-  <MonacoEditor
-    height="100%"
-    width="100%"
-    language={'javascript'}
-    value={dbmlCode}
-    theme="vs-dark"
-    onChange={onDbmlCodeChange}
-  />
-);
+const DBMLCodeEditor = ({ dbmlCode, onDbmlCodeChange }: DBMLCodeEditorProps) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleEditorDidMount = (editor: any, monaco: any) => {
+    monaco.languages.register({ id: 'dbml' });
+
+    monaco.languages.setMonarchTokensProvider('dbml', {
+      tokenizer: {
+        root: [
+          [/\b(Table|Ref|Project|Enum)\b/, 'keyword'],
+          [/\b(\w+)\b/, 'identifier'],
+          [/".*?"/, 'string'],
+          [/\/\/.*$/, 'comment'],
+          [/\[.*?\]/, 'annotation']
+        ]
+      }
+    });
+  };
+
+  return (
+    <MonacoEditor
+      height="100%"
+      width="100%"
+      language="dbml"
+      value={dbmlCode}
+      theme="vs-dark"
+      onChange={onDbmlCodeChange}
+      onMount={handleEditorDidMount}
+    />
+  );
+};
 
 export default DBMLCodeEditor;

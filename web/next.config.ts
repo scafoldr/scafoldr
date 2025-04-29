c99218f0d72ad89a70684ea976773cba8f1d9e04
Dimitrije Glibic <dimitrije.glibic1@gmail.com>
2025-04-29 21:25:14 +0200

    Update next.config.ts

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  // what URL segment your pages live under
-  basePath: '/scafoldr-web',
-  // where to load your _next assets from
-  assetPrefix: '/scafoldr-web/',
   webpack: (config) => {
     config.externals = [...config.externals, { canvas: 'canvas' }]; // required to make Konva & react-konva work
     return config;

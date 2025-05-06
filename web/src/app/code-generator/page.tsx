877ff80a1b768132d0eff6f3d5da646dbb8d0f37
Dimitrije Glibic <dimitrije.glibic1@gmail.com>
2025-05-06 22:48:43 +0200

    Update page.tsx

--- web/src/app/code-generator/page.tsx
+++ web/src/app/code-generator/page.tsx
@@ -1,12 +1,15 @@
 import Preview from '@/components/features/Preview/Preview';
+import Container from '@/components/layout/Container/Container';
 
 export default function CodeGenerator() {
   return (
     <div className="">
       <main className="">
-        <section id="scafoldr-code-generator" className="h-[100vh]">
-          <Preview />
-        </section>
+        <Container>
+          <section id="scafoldr-code-generator" className="h-[100vh]">
+            <Preview />
+          </section>
+        </Container>
       </main>
     </div>
   );

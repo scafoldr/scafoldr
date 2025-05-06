7f7b762b284009c13c5e3c0cf5b2c1da25bb84f9
Dimitrije Glibic <dimitrije.glibic1@gmail.com>
2025-05-06 22:44:46 +0200

    Add CodeGenerator as separate page

---
+++ web/src/app/code-generator/page.tsx
@@ -0,0 +1,13 @@
+import Preview from '@/components/features/Preview/Preview';
+
+export default function CodeGenerator() {
+  return (
+    <div className="">
+      <main className="">
+        <section id="scafoldr-code-generator" className="h-[100vh]">
+          <Preview />
+        </section>
+      </main>
+    </div>
+  );
+}

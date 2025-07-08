44e3506f078bfe6fefb63fa4392ea0f903bb4a2d
Dimitrije Glibic <dimitrije.glibic1@gmail.com>
2025-07-08 03:30:39 +0200

    Increase container width and add github link to header

--- web/src/components/layout/Container/Container.tsx
+++ web/src/components/layout/Container/Container.tsx
@@ -5,7 +5,7 @@ interface ContainerProps {
 }
 
 const Container = ({ children }: ContainerProps) => (
-  <div className="mx-auto max-w-screen-md px-3 md:px-0">{children}</div>
+  <div className="mx-auto max-w-screen-lg px-3 md:px-0">{children}</div>
 );
 
 export default Container;

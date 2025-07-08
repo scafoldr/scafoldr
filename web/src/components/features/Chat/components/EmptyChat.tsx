b101f6f73710287429443a5d980e8f4e3ebf523c
Dimitrije Glibic <dimitrije.glibic1@gmail.com>
2025-07-08 03:07:33 +0200

    Add guerrilla CTA for github stars

--- web/src/components/features/Chat/components/EmptyChat.tsx
+++ web/src/components/features/Chat/components/EmptyChat.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 const EmptyChat = () => (
   <div className="flex h-full flex-col justify-center items-center">
     <article className="prose">
@@ -6,6 +8,12 @@ const EmptyChat = () => (
       <p>🛠️ Edit: Hop into DBML view & fine-tune tables and columns. </p>
       <p>👀 Preview: Watch your ER-diagram spring to life.</p>
       <p>⚡ Generate: Click “Get your code” to spin up your backend. Ready to build? Let’s go!</p>
+      <p>
+        ⭐️ Star us on{' '}
+        <Link href="https://github.com/scafoldr/scafoldr" target="_blank" rel="noopener noreferrer">
+          github
+        </Link>
+      </p>
     </article>
   </div>
 );

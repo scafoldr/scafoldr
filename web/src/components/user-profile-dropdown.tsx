d289ef3440fb01a022de4acb06dc3dc1d8377397
Dimitrije Glibic <dimitrije.glibic1@gmail.com>
2025-08-01 23:20:50 +0200

    Remove canvas deps and fix console error

--- web/src/components/user-profile-dropdown.tsx
+++ web/src/components/user-profile-dropdown.tsx
@@ -29,7 +29,7 @@ export function UserProfileDropdown() {
         <DropdownMenuTrigger asChild>
           <Button variant="ghost" size="sm" className="relative h-8 w-8 rounded-full">
             <Avatar className="h-8 w-8">
-              <AvatarImage src="/avatars/01.png" alt="User" />
+              {/* <AvatarImage src="/avatars/01.png" alt="User" /> */}
               <AvatarFallback>
                 <User className="h-4 w-4" />
               </AvatarFallback>

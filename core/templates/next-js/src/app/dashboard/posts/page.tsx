8986014dcb313253a61b31d136ae08e4264b5c7e
Dimitrije Glibic <dimitrije.glibic1@gmail.com>
2025-08-06 00:42:56 +0200

    Bug fixes

--- core/templates/next-js/src/app/dashboard/posts/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { CrudPage } from '@/components/crud-page';
-import { postCrudConfig } from '@/features/posts/config';
-
-export default function PostsPage() {
-  return <CrudPage config={postCrudConfig} />;
-}
--- core/templates/next-js/src/app/dashboard/users/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { CrudPage } from '@/components/crud-page';
-import { userCrudConfig } from '@/features/users/config';
-
-export default function UsersPage() {
-  return <CrudPage config={userCrudConfig} />;
-}

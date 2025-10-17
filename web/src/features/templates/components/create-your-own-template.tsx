90adf5ec9dd2fd82075e42428581a70107e7a9f4
Dimitrije Glibic <dimitrije.glibic1@gmail.com>
2025-10-18 00:42:55 +0200

    Fix styles for template cards

--- web/src/features/templates/components/create-your-own-template.tsx
+++ web/src/features/templates/components/create-your-own-template.tsx
@@ -7,14 +7,16 @@ interface CreateYourOwnTemplateProps {
 
 const CreateYourOwnTemplate = ({ onClick }: CreateYourOwnTemplateProps) => (
   <Card
-    className={`bg-white dark:bg-slate-900 p-6 rounded-2xl border border-slate-200 dark:border-slate-800 shadow-sm hover:shadow-md transition-shadow`}
+    className={`bg-slate-200 dark:bg-slate-950 p-6 rounded-2xl border border-slate-200 dark:border-slate-800 shadow-sm hover:shadow-md transition-shadow hover:cursor-pointer `}
     onClick={onClick}>
     <div className="flex items-center justify-center h-[80px] py-2">
       <PencilRuler size={'100%'} />
     </div>
-    <h3 className="text-lg font-semibold mt-4 text-left">Create your own template</h3>
-    <p className="text-slate-600 dark:text-slate-400 text-xs text-left mt-2">
-      Upload your example project and scafoldr will create a custom template for you.
+    <h3 className="text-lg font-semibold mt-4 text-left flex gap-4 items-center justify-between">
+      Create your own template
+    </h3>
+    <p className="text-slate-600 dark:text-slate-400 text-xs text-left mt-2 h-8 line-clamp-2">
+      Upload your example project and Scafoldr will create a custom template for you.
     </p>
   </Card>
 );
--- web/src/features/templates/components/template-card.tsx
+++ web/src/features/templates/components/template-card.tsx
@@ -1,3 +1,4 @@
+import { Badge } from '@/components/ui/badge';
 import { Template } from '../types/template';
 import { Card } from '@/components/ui/card';
 
@@ -10,11 +11,20 @@ interface TemplateCardProps {
 const TemplateCard = ({ template, onClick, isActive = false }: TemplateCardProps) => {
   return (
     <Card
-      className={`bg-white dark:bg-slate-900 p-6 rounded-2xl border border-slate-200 dark:border-slate-800 shadow-sm hover:shadow-md transition-shadow ${isActive ? 'ring-2 ring-blue-500 dark:bg-slate-700' : ''}`}
+      className={`bg-white dark:bg-slate-900 p-6 rounded-2xl border border-slate-200 dark:border-slate-800 shadow-sm hover:shadow-md transition-shadow hover:cursor-pointer ${isActive ? 'ring-2 ring-blue-500 bg-slate-100 dark:bg-slate-700' : ''}`}
       onClick={onClick}>
       <div className="flex items-center justify-center h-[80px] py-2">{template.Icon}</div>
-      <h3 className="text-lg font-semibold mt-4 text-left">{template.name}</h3>
-      <p className="text-slate-600 dark:text-slate-400 text-xs text-left mt-2">
+      <h3 className="text-lg font-semibold mt-4 text-left flex gap-4 items-center justify-between">
+        {template.name}
+        {template.comingSoon && (
+          <Badge
+            variant="outline"
+            className="bg-orange-50 text-orange-700 border-orange-200 dark:bg-orange-900/20 dark:text-orange-400 dark:border-orange-800">
+            Coming Soon
+          </Badge>
+        )}
+      </h3>
+      <p className="text-slate-600 dark:text-slate-400 text-xs text-left mt-2 h-8 line-clamp-2">
         {template.description}
       </p>
     </Card>

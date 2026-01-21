3e13c4e47ed3398fe0a89ff38c0933f1761698ef
Ognjen Stojanovic <ostojanovic10021rn@raf.rs>
2026-01-21 18:37:49 +0100

    liquibase is set as the default for creating ddls

---
+++ rest/src/main/java/com/scafoldr/model/Project.java
@@ -0,0 +1,37 @@
+package com.scafoldr.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "project")
+public class Project extends DomainModel implements Serializable {
+
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_project",
+            joinColumns = @JoinColumn(name = "project_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private Set<User> users = new HashSet<>();
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+}
